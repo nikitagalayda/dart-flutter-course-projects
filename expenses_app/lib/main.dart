f55a5a0dd4618e16f38d83058a88d2e8471680ff
nikitagalayda <nikitagalayda@gmail.com>
2019-10-16 16:24:45 +0800

    add expenses_app

---
+++ expenses_app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              child: Container(
+                width: double.infinity,
+                child: Text('Chart'),
+              ),
+              elevation: 5,
+            ),
+            Card(
+              child: Text('Transactions'),
+            )
+          ],
+        ));
+  }
+}
